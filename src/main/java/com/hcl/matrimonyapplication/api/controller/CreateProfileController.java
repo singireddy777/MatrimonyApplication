230525b166c03ad0e0a69c00c733ffbd99c8fd19
shilendra <shilu1993sr@gmail.com>
2019-08-14 20:43:50 +0530

    Shilendra-2

--- src/main/java/com/hcl/matrimonyapplication/api/controller/CreateProfileController.java
+++ src/main/java/com/hcl/matrimonyapplication/api/controller/CreateProfileController.java
@@ -1,8 +1,11 @@
 package com.hcl.matrimonyapplication.api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +17,16 @@ import com.hcl.matrimonyapplication.api.service.CreateProfileService;
 
 @RestController
 @RequestMapping("/matrimony")
+@CrossOrigin(origins = "*")
 public class CreateProfileController {
+	private static final Logger logger = LoggerFactory.getLogger(CreateProfileController.class);
 	
 	@Autowired
 	private CreateProfileService createProfileService ;
 	
 	@PostMapping("/createProfile")
 	public ResponseEntity<ResponseDTO> createprofile(@RequestBody CreateProfileDTO ProfileProfileDTO){
+		logger.info("Create Profile METHOD--Controller!!!!!!!!!!!!!!!!!!");
 		
 		ResponseDTO ResponseDTO=createProfileService.create(ProfileProfileDTO);
 		return new ResponseEntity<>( ResponseDTO,HttpStatus.OK);
---
+++ src/test/java/com/hcl/matrimonyapplication/api/controller/CreateProfileControllerTest.java
@@ -0,0 +1,14 @@
+package com.hcl.matrimonyapplication.api.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreateProfileControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
