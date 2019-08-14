35e2cde6a8553279e85d4e9f2e6288be819afccd
venkat <venky.svreddy@gmail.com>
2019-08-14 21:49:03 +0530

    added tests

--- src/test/java/com/hcl/matrimonyapplication/api/controllertest/UserControllerTest.java
+++ src/test/java/com/hcl/matrimonyapplication/api/controllertest/UserControllerTest.java
@@ -1,18 +1,24 @@
 package com.hcl.matrimonyapplication.api.controllertest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.matrimonyapplication.api.controller.UserController;
+import com.hcl.matrimonyapplication.api.dto.UserDTO;
+import com.hcl.matrimonyapplication.api.dto.UserDetailDTO;
 import com.hcl.matrimonyapplication.api.entity.User;
 import com.hcl.matrimonyapplication.api.service.UserServiceImpl;
 
@@ -37,8 +43,18 @@ user.setPassword("password");
 	@Test
 	public void loginUserTest() throws Exception{
 		
-		mockmvc.perform(MockMvcRequestBuilders.post("/login").contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.ALL).content(asJsonString(user)));	
+		UserDTO userDTO = new UserDTO();
+		userDTO.setProfileId(97773L);
+		userDTO.setUserId(878);
+		userDTO.setUserName("vensee");
+		userDTO.setPassword("password");
+		
+		Mockito.when(userService.loginUser(Mockito.anyString(),Mockito.anyString())).thenReturn(userDTO);
+		
+		mockmvc.perform(MockMvcRequestBuilders.post("/matrimony/login").contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.ALL).content(asJsonString(user)));
+		ResponseEntity<UserDTO> message = userController.loginUser("12345", "password");
+		assertEquals(200, message.getStatusCodeValue());
 	}
 	public static String asJsonString(final Object object) {
 		try {
